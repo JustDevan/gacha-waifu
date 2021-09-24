43031aa1d8769c4124d80f4a6bf6287eb40e4e8e
JustDevan <justdevan1st@gmail.com>
2021-09-24 00:09:25 +0000

    Refactoring code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 const mainImage = document.getElementById('main-image')
-const generateButton = document.getElementById('gacha')
 const cards = document.getElementById('cards')
 
 const url = 'https://api.waifu.pics/sfw/waifu'
 const urlNSFW = 'https://api.waifu.pics/nsfw/waifu'
 
-const getImage = (url, success) => {
-  fetch(url).then(res => res.json()).then(res => success(res))
+const renderImage = (url) => {
+  fetch(url).then(res => res.json()).then(res => {
+    mainImage.setAttribute('src', res.url)
+  }
 }
 
 cards.addEventListener('click', el => {
   if (el.target.id == 'gacha') {
-    getImage(url, res => {
-      mainImage.setAttribute('src', res.url)
-    })
+    renderImage(url)
   } else if (el.target.id == 'nsfw') {
-    getImage(urlNSFW, res => {
-      mainImage.setAttribute('src', res.url)
-    })
+    renderImage(urlNSFW)
   }
 })
