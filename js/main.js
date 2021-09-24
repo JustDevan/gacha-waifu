18620c6e32418ef6971b50444f511e7f8ebf3f89
JustDevan <justdevan1st@gmail.com>
2021-09-24 05:30:10 +0000

    Penyederhanaan fungsi

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 const mainImage = document.getElementById('main-image')
 const cards = document.getElementById('cards')
 
-const url = 'https://api.waifu.pics/sfw/waifu'
-const urlNSFW = 'https://api.waifu.pics/nsfw/waifu'
-
-const renderImage = (url) => {
-  fetch(url).then(res => res.json()).then(res => {
-    mainImage.setAttribute('src', res.url)
+const renderImage = target => {
+  fetch(`https://api.waifu.pics/${target}/waifu`).then(res => res.json()).then(res => {
+    mainImage.setAttribute('src',  res.url)
   }
 }
 
 cards.addEventListener('click', el => {
-  if (el.target.id == 'gacha') {
-    renderImage(url)
+  if (el.target.id == 'sfw') {
+    renderImage(el.target.id)
   } else if (el.target.id == 'nsfw') {
-    renderImage(urlNSFW)
+    renderImage(el.target.id)
   }
 })
